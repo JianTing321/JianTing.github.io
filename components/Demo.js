ddcc769503a6aa14bc92effda2ddf64be351a731
JianTing <jason717011@eip.chuanhwa.com.tw>
2023-03-21 10:24:39 +0800

    1

--- components/Demo.js
+++ components/Demo.js
@@ -2,10 +2,19 @@ import { createApp, ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, on
 
 export default {
     name: 'Demo',
-    template: `
+    template: 
+    `
     <div>我是Demo</div>
     <div>{{sum}}</div>
     <button @click="sum++">點擊</button>    
+   
+    <el-switch v-model="value1" />
+    <el-switch
+      v-model="value2"
+      class="ml-2"
+      style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+    />
+
     `,
     setup() {
         console.log('---setup---')
